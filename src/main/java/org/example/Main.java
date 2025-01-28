5b1ab9f3858081e518ad2d06529b79b6226ccf63
Ergi Dibra <ergdib18@htl-shkoder.com>
2025-01-28 11:09:49 +0100

    Erste Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    private static final String url = "jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:5432/postgres";
+    private static final String user = "postgres.quumbrmumjrkybgyvvgq";
+    private static final String password = "Passwordprdatabase123!";
+
+    public static void main(String[] args) {
+
+        String query = "SELECT title, description, published_at FROM books ORDER BY title";
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            System.out.println("Books  by title:");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("title"));
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    }
+
+
+
+
